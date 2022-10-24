f9fb318fd856333a214141ffd401642c3636c522
Muhamed Šljivić <81625044+muhamedsljivic@users.noreply.github.com>
2022-10-24 16:00:15 +0200

    Add files via upload

---
+++ Week1.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Week1 {
+
+	public static void main(String[] args) {
+// 1.	System.out.println("Muhamed Sljivic");
+		
+// 2.	System.out.println("Hello world!\n(And  all the people of the world)");
+	
+// 3.	Scanner sc = new Scanner(System.in);
+// 	System.out.println("Year: ");
+//	int years = sc.nextInt();
+//	int seconds = 3600 * years;
+	
+//	System.out.println("There are " + seconds + " in a year");
+		
+// 4.		System.out.println("Type a number: ");
+//		Scanner sc = new Scanner(System.in);
+//		int num1 = sc.nextInt();
+	//	System.out.println("Type another number: ");
+//   	int num2 = sc.nextInt();
+		
+//		System.out.println("Sum of the numbers: " + (num1 + num2));
+		
+//5.		System.out.println("Type a number: ");
+//		Scanner sc = new Scanner(System.in);
+	//	int number = sc.nextInt();
+		
+//		if(number > 0) {
+	//		System.out.println("The number is positive");
+	//	} else if(number < 0) {
+		//	System.out.println("The number is negative");
+//		} else {
+//			System.out.println("The number is 0");
+		//}
+		
+//	6.	System.out.println("Type the first number: ");
+//		Scanner sc = new Scanner(System.in);
+//		int num1 = sc.nextInt();
+	//	System.out.println("Type the second number: ");
+//		int num2 = sc.nextInt();
+		
+//		if(num1 == num2) {
+//			System.out.println("The numbers are equal.");
+//		} else if(num1 > num2) {
+//			System.out.println("Greater number: " + num1);
+//		} else {
+//		System.out.println("Greater number: " + num2);
+//	}
+	}
+
+}
+
+
